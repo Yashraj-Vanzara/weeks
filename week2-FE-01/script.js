acfcc7cbd046da357b3f531823e36c394e7b3515
Yashraj Vanzara <vanzarayashraj@gmail.com>
2025-12-14 18:34:31 +0530

    feat: learn JavaScript console methods

---
+++ week2-FE-01/script.js
@@ -0,0 +1,14 @@
+// ! console basics
+
+// ? Console is used to console(print) something on console
+console.log("Hello world")  
+//* There are various types of consoles
+
+// ! Types
+
+
+// ? Used for warning
+console.warn("Warning")
+
+// !used for error
+console.error("Error")
