b75534b24d15395fba72f1b13a8c6b754ef05fc6
Yashraj Vanzara <vanzarayashraj@gmail.com>
2025-12-14 18:37:48 +0530

    feat:learn the varibles

--- week2-FE-01/script.js
+++ week2-FE-01/script.js
@@ -12,3 +12,12 @@ console.warn("Warning")
 
 // !used for error
 console.error("Error")
+
+
+// ! variables
+
+// * can change the value of let
+let name="yashraj" 
+
+// * cannot change the value of const
+const id="784sd8sd48"
