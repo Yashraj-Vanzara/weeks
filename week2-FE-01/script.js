3d24163f47ee0e44ed4fdf93393b36ab0beb4a59
Yashraj Vanzara <vanzarayashraj@gmail.com>
2025-12-14 18:51:15 +0530

    feat:learn primitive types and string methods

--- week2-FE-01/script.js
+++ week2-FE-01/script.js
@@ -21,3 +21,44 @@ let name="yashraj"
 
 // * cannot change the value of const
 const id="784sd8sd48"
+
+
+// ! Data types primitive
+
+// * String,Numbers,Boolean,null,undefined
+
+const fname="Yashraj"
+const number=50
+const isRizzer=true
+const value=null  
+const successRate=undefined
+
+
+//  ? To test check the type of variable use typeof
+
+// * Example
+
+console.log(typeof isRizzer)
+
+// ! most imp Type of null is object
+
+// * Concatenation
+
+const lname=riffer
+const NAme=john
+
+// ? this is es6 method template string
+console.log(` the full name is ${NAme} ${lname}`)
+
+// ! String Methods
+
+const RandomWord="Hello World"
+
+// ? Gives length of string
+console.log(RandomWord.length)
+
+// Similar methods are 
+// ! RandomWord.toUpperCase()
+// ! RandomWord.toLowerCase()
+//  !RandomWord.substring(0,8)  it will take upto 8-1 =7 so (0,7)
+//  !RandomWord.split()
