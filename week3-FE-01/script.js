969c452bb89bdb821657e64d6052f2e6f8030dde
Yashraj Vanzara <vanzarayashraj@gmail.com>
2025-12-17 23:02:17 +0530

    feat:Completed Lesson1

---
+++ week3-FE-01/script.js
@@ -0,0 +1,9 @@
+// ! DOM INTRO
+
+/*
+DOM is document object model
+created by the browser when a web page is loaded
+in graphical form it looks like a tree of elements/nodes
+we use js to read and meanipulate dom
+
+*/
