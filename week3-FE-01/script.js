aed1fcce9e4526a605951c967f9884644957df1e
Yashraj Vanzara <vanzarayashraj@gmail.com>
2025-12-17 23:23:18 +0530

    feat:lesson3 completed

--- week3-FE-01/script.js
+++ week3-FE-01/script.js
@@ -11,3 +11,11 @@ we use js to read and meanipulate dom
 // ! Get Element By id method --Lesson2
 
 const pageBanner=document.getElementById('page-banner')
+
+// !Get elements by class or tag --Lesson3
+
+const h1=document.getElementsByClassName('title')
+const li =document.getElementsByTagName('li')
+
+// ! How to convert a anything in array
+//?  Array.from(h1)
