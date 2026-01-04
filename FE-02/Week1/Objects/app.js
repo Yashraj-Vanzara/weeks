e8d279ccbc4ecb231971fdaea4fd85da93805429
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-01-04 19:18:15 +0530

    feat:learn new way to store objects in array

--- FE-02/Week1/Objects/app.js
+++ FE-02/Week1/Objects/app.js
@@ -1,37 +1,45 @@
 //! objects literals
 
-let user={
-    name:"crystal",
-    age:30,
-    email:"crrystal@thenetninja.co.uk",
-    location:"berlin",
-    blogs:["mac & cheese","Hello World"],
-    login:function(){
-        console.log('user logged in')
-    },
-    logout:function(){
-         console.log('user logged  out')
+// const blogs=[
 
-    },
+//     {title:"why mac & cheese",likes:30},
+//     {title:"life of nerd",likes:300},
+// ]
 
-    logblogs:function(){
-        console.log("This user have written this blogs")
-        this.blogs.forEach(blog=>{console.log(blog)})
-    }
-    // !This refers to blogs
-    
-}
+let user = {
+  name: "crystal",
+  age: 30,
+  email: "crrystal@thenetninja.co.uk",
+  location: "berlin",
+  blogs: [
+    { title: "why mac & cheese", likes: 30 },
+    { title: "life of nerd", likes: 300 },
+  ],
+  login: function () {
+    console.log("user logged in");
+  },
+  logout: function () {
+    console.log("user logged  out");
+  },
+
+  logblogs: function () {
+    console.log("This user have written this blogs");
+    this.blogs.forEach((blog) => {
+      console.log(blog.title,blog.likes);
+    });
+  },
+  // !This refers to blogs
+};
 
 // * To access any of these
-console.log(user)
-console.log(user.name)
+console.log(user);
+console.log(user.name);
 
 //  ?To update
 // user.age=35
 
-
 // * access using square bracket
-console.log(user["name"])
+console.log(user["name"]);
 
 //  ?To update
 // user["name"]="chun-li"
@@ -41,9 +49,3 @@ console.log(user["name"])
 // user[key]
 
 // ! Type of object is object
-
-
-
-
-
-
