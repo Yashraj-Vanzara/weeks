76b0c5cdca0f9560c084baf40b15991ed96a3a1e
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-01-04 19:00:41 +0530

    fixed:Folder Structure

---
+++ FE-02/Week1/Objects/app.js
@@ -0,0 +1,30 @@
+//! objects literals
+
+let user={
+    name:"crystal",
+    age:30,
+    email:"crrystal@thenetninja.co.uk",
+    location:"berlin",
+    blogs:["mac & cheese","Hello World"]
+}
+
+// * To access any of these
+console.log(user)
+console.log(user.name)
+
+//  ?To update
+// user.age=35
+
+
+// * access using square bracket
+console.log(user["name"])
+
+//  ?To update
+// user["name"]="chun-li"
+
+// ! To pass string as key
+// const key="lat"
+// user[key]
+
+// ! Type of object is object
+
