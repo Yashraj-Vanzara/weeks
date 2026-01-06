5a828d7087764f00484a3150beee3131a760a9d2
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-01-06 23:57:28 +0530

    fixed :The Printing Statements

--- FE-02/Week2/Project/app.js
+++ FE-02/Week2/Project/app.js
@@ -1,39 +1,43 @@
-const { exec } = require('child_process')
-const fs=require('fs')
+const { exec } = require("child_process");
+const fs = require("fs");
 
-function genrateAnalysisFromEmployes(){
-    // const newreport=[]
-    const Employes=fs.readFileSync('./employes.json',"utf-8")
-    const data=(JSON.parse(Employes))
-    const result=data.sort((a,b)=>b.salary-a.salary)
-    // !step 1 done
-    // console.log("The list of sorted Employess",result)
+function genrateAnalysisFromEmployes() {
 
-    const expemployee=data.filter(x=>x.experience>=3)
-    // console.log('---------------------------------------------------------------------------------------')
-    console.log('xp > 3',expemployee)
+  const Employes = fs.readFileSync("./employes.json", "utf-8");
+  const data = JSON.parse(Employes);
+  const result = data.sort((a, b) => b.salary - a.salary);
 
-    const summarylist=[]
-    expemployee.forEach(element => {
-        const anualbonus=element.salary*0.10*element.experience
-        summarylist.push({name:element.name,department:element.department,anualbonus})
 
+  const expemployee = data.filter((x) => x.experience >= 3);
+
+
+  const summarylist = [];
+  expemployee.forEach((element) => {
+    const anualbonus = element.salary * 0.1 * element.experience;
+    summarylist.push({
+      name: element.name,
+      department: element.department,
+      anualbonus,
     });
+  });
 
-    console.log(summarylist)
+  const totalSalryExpenditure = expemployee.reduce((acc, curr) => {
+    acc += curr.salary;
+    return acc;
+  }, 0);
 
-  
 
-    const totalSalryExpenditure=expemployee.reduce((acc,curr)=>{
-                acc+=curr.salary
-                return acc
-    },0)
 
-    console.log(totalSalryExpenditure)
-        
+  console.log(" Step-1 The list of sorted Employess\n", result);
+  console.log(
+    "---------------------------------------------------------------------------------------"
+  );
+  console.log("\nStep-2 The Summary List\n", summarylist);
+  console.log(
+    "---------------------------------------------------------------------------------------"
+  );
 
-   
+    console.log("\nStep-3 The Total Salary Expenditure\n",totalSalryExpenditure);
 }
 
-
-genrateAnalysisFromEmployes()
+genrateAnalysisFromEmployes();
