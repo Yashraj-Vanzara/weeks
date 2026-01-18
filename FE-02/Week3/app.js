c73095b68e2581eeb606f56e6e2714fa5ba87579
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-01-18 23:41:33 +0530

    feat:completed

--- FE-02/Week3/app.js
+++ FE-02/Week3/app.js
@@ -6,28 +6,76 @@ const line = data.split("\n");
 const headers = line[0].trim().split(",");
 console.log(headers);
 
-const transactions=[]
+const transactions = [];
 
 for (let index = 1; index < line.length; index++) {
-   
-    const obj={}
+  const obj = {};
 
-    const data=line[index].split(',')
- headers.forEach((value,index)=>{
-    obj[value]=data[index]
- })
+  const data = line[index].split(",");
+  headers.forEach((value, index) => {
+    obj[value] = data[index];
+  });
 
- obj.Amount=Number(obj.Amount)
+  obj.Amount = Number(obj.Amount);
 
-    transactions.push(obj)
-
-
+  transactions.push(obj);
 }
 
 //! sort by date
 
-transactions.sort((a,b)=>(new Date(a.Date)-new Date(b.Date)))
+transactions.sort((a, b) => new Date(a.Date) - new Date(b.Date));
+
+const summary = {};
+
+for (const txs of transactions) {
+  const name = txs.AccountHolder;
+
+  if (!summary[name]) {
+    summary[name] = {
+      AccountHolder: name,
+      TotalCredit: 0,
+      TotalDebit: 0,
+      LargestTransaction: 0,
+      SalaryTransactions: [],
+    };
+  }
+
+  if (txs.Type === "Credit") {
+    summary[name].TotalCredit += txs.Amount;
+  } else {
+    summary[name].TotalDebit += txs.Amount;
+  }
+
+  if (Math.abs(txs.Amount) > summary[name].LargestTransaction) {
+    summary[name].LargestTransaction = Math.abs(txs.Amount);
+  }
+
+  if (txs.Remarks.toLowerCase().includes("salary")) {
+    summary[name].SalaryTransactions.push(txs.TransactionID);
+  }
+}
+
+const summaryArray = Object.values(summary);
+const headerss = [
+  "AccountHolder",
+  "TotalCredit",
+  "TotalDebit",
+  "LargestTransaction",
+  "SalaryTransactions",
+];
+
+let csv = headerss.join(",") + "\n";
 
 
+summaryArray.forEach((user) => {
+  const row = [
+    user.AccountHolder,
+    user.TotalCredit,
+    user.TotalDebit,
+    user.LargestTransaction,
+    `"${user.SalaryTransactions.join(";")}"`,
+  ];
+  csv += row.join(",") + "\n";
+});
 
-console.log( transactions)
+fs.writeFileSync("bank_summary.csv", csv, "utf-8");
