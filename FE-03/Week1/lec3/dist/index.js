fc89f70b01d632424e01d10282a44c2489b00acb
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-01-20 18:35:56 +0530

    feat:learn how to install ts

---
+++ FE-03/Week1/lec3/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `Hello ${person}`;
+}
+const user = "Preet";
+console.log(greet(user));
+//# sourceMappingURL=index.js.map
---
+++ FE-03/Week1/lec3/src/index.ts
@@ -0,0 +1,6 @@
+function greet(person:string):string{
+    return `Hello ${person}`
+}
+
+const user:string="Preet"
+console.log(greet(user))
