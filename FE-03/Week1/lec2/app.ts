4c8b67dfdcfa933582ef7f2de83a6959a1b3044b
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-01-20 18:24:28 +0530

    feat:Behind The Scence of ts

---
+++ FE-03/Week1/lec2/app.ts
@@ -0,0 +1,33 @@
+// How TS Code Works?
+
+
+
+// TS Code -> Lexer -> Parser -> Binder -> Checker -> Emmiter -> .js, .d.ts, .map
+
+
+
+// Lexer -> Tokenize karna 
+
+
+
+// Parser -> AST Explorer -> Puri language ke through ek tree banate hai
+
+
+
+// Binder -> Binder AST ko leta hai
+
+// Roles: Symbol Tables
+
+//        Parent Pointer
+
+//        Flow Nodes
+
+
+
+// Checker -> Har ek node par check karta hai (Syntax Check)
+
+// (Short Circuit)
+
+
+
+// Emmiter -> File ko generate karta hi fir, typescript ne jo bhi add kia usko remove karta hai (ye Node karta hai)
