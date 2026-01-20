ecffef3adc705b94e19fa9aaed2679af760ef855
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-01-20 17:58:29 +0530

    feat:lec 6 done

---
+++ FE-02/Week3/Lectures/lec6/app.js
@@ -0,0 +1,27 @@
+console.log(a)
+var a
+console.log(x)
+// a=7
+
+if(a===undefined){
+    console.log("a is undefined")
+}
+else{
+    console.log("a is not undefined")
+}
+
+
+// 1. Undefined is like a placeholder till a variable is not assigned a value.
+// 2. undefined !== not defined
+// 3. JS- weakly typed language since it doesn't depend on data type declarations.
+
+// ! loosely type
+
+var a=10
+console.log(a)
+a="hello"
+console.log(a)
+
+// ! Dont do this never not a got practice to assign undefined to any variable 
+
+a=undefined
