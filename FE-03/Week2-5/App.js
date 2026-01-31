5a5dc8f81168d862494f6dc8aed342bbca2c8e6a
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-01-31 21:25:00 +0530

    feat:learn how to give attributes

--- FE-03/Week2-5/App.js
+++ FE-03/Week2-5/App.js
@@ -1,5 +1,5 @@
-  <script>
-         const heading =React.createElement("h1", {}, "Hello world");
-         const root = ReactDOM.createRoot(document.getElementById("root"));
-         root.render(heading);
-    </script>
+// ! {} this is where we put attributes for our tag
+
+const heading = React.createElement("h1", {id:"Parent"}, "Hello world");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
