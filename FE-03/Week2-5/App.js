fa12b1c822ee0ad107ee54947645b1e80c47d71d
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-01-31 21:22:35 +0530

    refactor:Changed Folder Structure

---
+++ FE-03/Week2-5/App.js
@@ -0,0 +1,5 @@
+  <script>
+         const heading =React.createElement("h1", {}, "Hello world");
+         const root = ReactDOM.createRoot(document.getElementById("root"));
+         root.render(heading);
+    </script>
