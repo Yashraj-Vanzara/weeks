e1a19f75083b8b18e934e85dd2591d6196eba4fa
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-01-31 21:30:50 +0530

    feat:learn CreateElement gives object

--- FE-03/Week2-5/App.js
+++ FE-03/Week2-5/App.js
@@ -1,5 +1,6 @@
 // ! {} this is where we put attributes for our tag
 
 const heading = React.createElement("h1", {id:"Parent"}, "Hello world");
+// heading is object also createElement gives us object where {} is attribute and hello world is children and whole thing is props
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
