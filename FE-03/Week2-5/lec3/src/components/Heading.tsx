63d32fd51a0a1555d9cd30b47d9ee3bc073f8c31
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-02-01 20:31:44 +0530

    feat:Passed another componet into another one

--- FE-03/Week2-5/lec3/src/components/Heading.tsx
+++ FE-03/Week2-5/lec3/src/components/Heading.tsx
@@ -1,10 +1,20 @@
+import type React from "react"
 import type { JSX } from "react"
 
 
-const Heading:React.FC = () :JSX.Element => {
+const Heading:React.FC = (props:any) :JSX.Element => {
   return (
-    <div>This is a functional component</div>
+    <div id="container"><h1><>{props.Title}</></h1></div>
   )
 }
+
+export const Title:React.FC=():JSX.Element=>{
+    return(<div>
+            <h1>Im from title</h1>
+        </div>
+    )
+}
+
+
 
 export default Heading
