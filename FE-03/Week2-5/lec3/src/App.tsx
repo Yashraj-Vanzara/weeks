41e174094f9fb8e04e5e5626c3cedb44371a6937
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-02-01 20:08:16 +0530

    fix:Learn more about jsx

--- FE-03/Week2-5/lec3/src/App.tsx
+++ FE-03/Week2-5/lec3/src/App.tsx
@@ -1,5 +1,7 @@
 
 //! jsx is html like syntax not html in javascript
+// ! if we want to write multiple lines of jsx we have to wrap it in parentheses
+// ! we can only return one parent element in jsx if we want to return multiple elements we have to wrap them in a parent element like div or fragment
 const App = () => {
   return (
     <div>App</div>
