2b3c0077bdab0cb93e65e83e41eaf33abf65b922
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-02-01 20:15:55 +0530

    feat:made a component

--- FE-03/Week2-5/lec3/src/App.tsx
+++ FE-03/Week2-5/lec3/src/App.tsx
@@ -1,10 +1,13 @@
 
 //! jsx is html like syntax not html in javascript
 // ! if we want to write multiple lines of jsx we have to wrap it in parentheses
+
+import Heading from "./components/Heading"
+
 // ! we can only return one parent element in jsx if we want to return multiple elements we have to wrap them in a parent element like div or fragment
 const App = () => {
   return (
-    <div>App</div>
+    <div><Heading/></div>
   )
 }
 
---
+++ FE-03/Week2-5/lec3/src/components/Heading.tsx
@@ -0,0 +1,10 @@
+import type { JSX } from "react"
+
+
+const Heading:React.FC = () :JSX.Element => {
+  return (
+    <div>This is a functional component</div>
+  )
+}
+
+export default Heading
