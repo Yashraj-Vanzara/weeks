8b76229e4c94c54a6fb53faa74fb2d19b1e32cc2
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-02-08 01:00:25 +0530

    feat:Added logout and login

--- FE-03/Week2-5/FoodDeliveryApp/src/components/Navbar.tsx
+++ FE-03/Week2-5/FoodDeliveryApp/src/components/Navbar.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react"
 
 
 const Navbar: React.FC = () => {
+  const[btnname,setbtnname]=useState("Login")
   return (
     <div className="Navbar">
         <ul>
@@ -8,6 +10,9 @@ const Navbar: React.FC = () => {
             <li>About</li>
             <li>Contact</li>
             <li>Cart</li>
+            <button onClick={()=>{
+              btnname==="Login"?setbtnname("Logout"):setbtnname("Login")
+            }} style={{padding:"2px 10px",backgroundColor:"transparent",cursor:"pointer"}}>{btnname}</button>
         </ul>
     </div>
   )
