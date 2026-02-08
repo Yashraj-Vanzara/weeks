adf0d090cf0c6ec9d67f9301fd394509051835a9
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-02-08 15:56:55 +0530

    feat:Added State variables

--- FE-03/Week2-5/FoodDeliveryApp/src/components/UserClass.tsx
+++ FE-03/Week2-5/FoodDeliveryApp/src/components/UserClass.tsx
@@ -4,10 +4,18 @@ import React from "react"
 class UserClass extends React.Component{
     constructor(props){
         super(props)
+
+        this.state={
+            count:0,
+            count2:2
+        }
     }
     render(): ReactNode {
+        const{count,count2}=this.state
         return (
             <div>
+                <p>{count}</p>
+                <p>{count2}</p>
                 <h2>{this.props.name}</h2>
                 <p>Location</p>
                 <h2>Email</h2>
