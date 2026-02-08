fbf8f2585cb8f02ba4ebd337c963a17736790c35
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-02-08 12:39:00 +0530

    feat:Added route

--- FE-03/Week2-5/FoodDeliveryApp/src/App.tsx
+++ FE-03/Week2-5/FoodDeliveryApp/src/App.tsx
@@ -1,5 +1,7 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import BodyContainer from './components/BodyContainer'
 import Header from './components/Header'
+import About from './components/About'
 const App = () => {
   return (
     <div className="App">
@@ -9,4 +11,15 @@ const App = () => {
   )
 }
 
+
+export const appRouter=createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>
+  },
+  {
+    path:"/about",
+    element:<About/>
+  }
+])
 export default App
