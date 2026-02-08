d6df0221ac4a50fcc0b904b7a89fbe600430a795
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-02-08 12:39:46 +0530

    feat:Added Contact page

--- FE-03/Week2-5/FoodDeliveryApp/src/App.tsx
+++ FE-03/Week2-5/FoodDeliveryApp/src/App.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import BodyContainer from './components/BodyContainer'
 import Header from './components/Header'
 import About from './components/About'
+import Contact from './components/Contact'
 const App = () => {
   return (
     <div className="App">
@@ -20,6 +21,10 @@ export const appRouter=createBrowserRouter([
   {
     path:"/about",
     element:<About/>
+  },
+  {
+    path:"/contact",
+    element:<Contact/>
   }
 ])
 export default App
