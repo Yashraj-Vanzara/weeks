103bbc9b330a8c67e16cee9d87e3a42ce4c44814
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-02-10 01:20:44 +0530

    feat:Styles

--- FE-03/Week2-5/FoodDeliveryApp/src/components/RestaurantCard.tsx
+++ FE-03/Week2-5/FoodDeliveryApp/src/components/RestaurantCard.tsx
@@ -7,7 +7,7 @@ type RestaurantCardProps = {
 };
 const RestaurantCard: React.FC<RestaurantCardProps> = (props) => {
   return (
-    <div className="w-50 p-4 h-auto bg-slate-100 rounded-2xl ">
+    <div className="w-50 p-4 h-100 bg-slate-100 rounded-2xl ">
         <img className="rounded-xl" src={props.imageUrl || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRexBia36ixX_U4FoPlg0iqv2hZIvfRyeH8LQ&s"} alt="image" />
         <h3>{props.Name || "Meghna"}</h3>
         <p>{props.cuisine || "North Indian, Chinese"}</p>
