a8eb6ed2cc5dd809500fc22b6afe2ef231f6f3e4
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-03-11 10:53:38 +0530

    feat:Header done

--- FE-03/Week2-5/netflixgpt/src/components/Header.tsx
+++ FE-03/Week2-5/netflixgpt/src/components/Header.tsx
@@ -2,7 +2,9 @@
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="px-16 py-10 absolute z-99">
+        <img className="w-44 h-auto  object-cover" src="/netflix.svg" alt="netflixlogo" />
+    </div>
   )
 }
 
--- FE-03/Week2-5/netflixgpt/src/components/Login.tsx
+++ FE-03/Week2-5/netflixgpt/src/components/Login.tsx
@@ -1,8 +1,16 @@
+import Header from "./Header"
 
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <Header/>
+      <div className="w-full ">
+        <div className="absolute z-9 bg-black/48 w-full h-screen">
+        </div>
+        <img className="w-full h-screen object-cover" src="https://assets.nflxext.com/ffe/siteui/vlv3/b9448d14-5983-4ffc-a4d6-e22223108466/web/IN-en-20260302-TRIFECTA-perspective_1ef91182-c674-4632-9bec-d185993ab154_large.jpg" alt="" />
+      </div>
+    </div>
   )
 }
 
