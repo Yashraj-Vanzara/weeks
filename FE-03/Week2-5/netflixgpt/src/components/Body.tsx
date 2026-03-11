eb9d4556821a45dac08de619dead58e11b48df82
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-03-11 10:20:39 +0530

    feat:Added Route

--- FE-03/Week2-5/netflixgpt/src/components/Body.tsx
+++ FE-03/Week2-5/netflixgpt/src/components/Body.tsx
@@ -1,12 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Browse from "./Browse"
 import Login from "./Login"
 
+
 const Body = () => {
+  
+const appRouter=createBrowserRouter([
+
+  {
+    path:"/",
+    element:<Login/>
+  },
+  {
+    path:"/browse",
+    element:<Browse/>
+  }
+])
+
   return (
-    <div>
-     <Login />
-     <Browse/>
-    </div>
+    <RouterProvider router={appRouter}/>
   )
 }
 
