f3b531f55a4b8a044203bf7e992be62a2b958e86
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-03-13 22:04:53 +0530

    feat: refactor Browse component to use custom hook for fetching now playing movies

--- FE-03/Week2-5/netflixgpt/src/components/Browse.tsx
+++ FE-03/Week2-5/netflixgpt/src/components/Browse.tsx
@@ -1,26 +1,12 @@
-import { useEffect } from "react"
+
+import useNowPlaying from "../hooks/useNowPlaying"
 import Header from "./Header"
-import { options,url } from "../constants/constant"
-import { useDispatch } from "react-redux"
-import { addNowPlayingMovies } from "../utils/movieSlice"
+
 
 
 const Browse = () => {
-  const dispatch=useDispatch()
-  // Api call 
-const getNowPlaying= async ()=>{
 
-    const res=await fetch(url,options)
-    const json=await res.json()
-    console.log("Now playing",json.results)
-    dispatch(addNowPlayingMovies(json.results))
-
-
-  }
-  useEffect(()=>{
-    getNowPlaying()
-  },[])
-
+useNowPlaying()
   
   return (
     <div>
---
+++ FE-03/Week2-5/netflixgpt/src/hooks/useNowPlaying.ts
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { addNowPlayingMovies } from "../utils/movieSlice";
+import { useDispatch } from "react-redux";
+import { options, url } from "../constants/constant";
+
+export default function useNowPlaying() {
+  const dispatch=useDispatch()
+  // Api call 
+const getNowPlaying= async ()=>{
+
+    const res=await fetch(url,options)
+    const json=await res.json()
+    console.log("Now playing",json.results)
+    dispatch(addNowPlayingMovies(json.results))
+
+
+  }
+  useEffect(()=>{
+    getNowPlaying()
+  },[])
+}
