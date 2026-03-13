dfa3fbda3c9b4070ad7a3ca6cef5630544989819
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-03-13 21:45:23 +0530

    feat: Implement movie fetching and state management

--- FE-03/Week2-5/netflixgpt/src/components/Browse.tsx
+++ FE-03/Week2-5/netflixgpt/src/components/Browse.tsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react"
 import Header from "./Header"
+import { options,url } from "../constants/constant"
 
 
 const Browse = () => {
+  // Api call 
+const getNowPlaying= async ()=>{
+
+    const res=await fetch(url,options)
+    const json=await res.json()
+    console.log("Now playing",json)
+
+  }
+  useEffect(()=>{
+    getNowPlaying()
+  },[])
+
+  
   return (
     <div>
       <Header/>
---
+++ FE-03/Week2-5/netflixgpt/src/constants/constant.ts
@@ -0,0 +1,10 @@
+
+export const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMTc1YzkyMjE3NTk4ODVjZDNhMWRkMjliNzczMTgwMiIsIm5iZiI6MTc3MzI1MjI0Ni41ODIsInN1YiI6IjY5YjFhZTk2YzM4ODk4NWYxMjIxY2U0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KXufGwo6LQjt7i60h2dC0FCI8wW9F2gU1wEJARmhOFA'
+  }
+};
+
+export const url = 'https://api.themoviedb.org/3/movie/now_playing';
