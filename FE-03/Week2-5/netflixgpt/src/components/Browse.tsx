fb5c3cc904e66b74c24e57642a103160e3aa5fdf
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-03-13 21:51:01 +0530

    feat: dispatched the action

--- FE-03/Week2-5/netflixgpt/src/components/Browse.tsx
+++ FE-03/Week2-5/netflixgpt/src/components/Browse.tsx
@@ -1,15 +1,20 @@
 import { useEffect } from "react"
 import Header from "./Header"
 import { options,url } from "../constants/constant"
+import { useDispatch } from "react-redux"
+import { addNowPlayingMovies } from "../utils/movieSlice"
 
 
 const Browse = () => {
+  const dispatch=useDispatch()
   // Api call 
 const getNowPlaying= async ()=>{
 
     const res=await fetch(url,options)
     const json=await res.json()
-    console.log("Now playing",json)
+    console.log("Now playing",json.results)
+    dispatch(addNowPlayingMovies(json.results))
+
 
   }
   useEffect(()=>{
