7c6a38213b44c9867ac23496269ff570a4e87a0c
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-03-16 20:19:50 +0530

    feat: enhance VideoContainer to fetch and display video trailers using Redux

--- FE-03/Week2-5/netflixgpt/src/components/VideoContainer.tsx
+++ FE-03/Week2-5/netflixgpt/src/components/VideoContainer.tsx
@@ -1,9 +1,14 @@
 import { useEffect } from "react"
 import { options } from "../constants/constant"
+import { useDispatch, useSelector } from "react-redux"
+import { addTrailer } from "../utils/movieSlice"
 
 
 //@ts-expect-error : if not number
 const VideoContainer = ({id}) => {
+  const dispatch=useDispatch()
+   //@ts-expect-error :if not store
+  const strore=useSelector((store)=>store.movie?.nowPlayingTailer)
   //@ts-expect-error : if not number
    const getVideo= async (id)=>{
 console.log("infn",id)
@@ -14,6 +19,7 @@ console.log("infn",id)
     //@ts-expect-error:nothing
     const filterdata=json.results.filter(vid=>vid.type==="Trailer")
     const movtrailer=filterdata.length?filterdata[0] : json.results[0]
+    dispatch(addTrailer(movtrailer))
     console.log("fd",filterdata,movtrailer)
    }
 
@@ -22,7 +28,7 @@ console.log("infn",id)
    },[])
   return (
     <div className="w-full h-screen">
-      <video className="w-full " src="https://www.youtube.com/watch?v=AFuE1LRxm80" autoPlay loop ></video>
+      <iframe className="w-screen h-screen" src={`https://www.youtube.com/embed/${strore?.key}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
     </div>
   )
 }
--- FE-03/Week2-5/netflixgpt/src/utils/movieSlice.ts
+++ FE-03/Week2-5/netflixgpt/src/utils/movieSlice.ts
@@ -3,13 +3,17 @@ import { createSlice } from "@reduxjs/toolkit";
 const movieSlice=createSlice({
     name:"movies",
     initialState:{
-        nowPlayingMovies:null
+        nowPlayingMovies:null,
+        nowPlayingTailer:null
     },
     reducers:{
         addNowPlayingMovies:(state,action)=>{
             state.nowPlayingMovies=action.payload
+        },
+        addTrailer:(state,action)=>{
+            state.nowPlayingTailer=action.payload
         }
     }
 })
-export const {addNowPlayingMovies} = movieSlice.actions
+export const {addNowPlayingMovies,addTrailer} = movieSlice.actions
 export default movieSlice.reducer
