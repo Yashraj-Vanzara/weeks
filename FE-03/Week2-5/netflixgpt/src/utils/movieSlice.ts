c913e527e07ec782158d4b27838c2e7cf6f5494b
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-03-17 22:10:55 +0530

    feat:Added new movie Genre

--- FE-03/Week2-5/netflixgpt/src/utils/movieSlice.ts
+++ FE-03/Week2-5/netflixgpt/src/utils/movieSlice.ts
@@ -1,19 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const movieSlice=createSlice({
-    name:"movies",
-    initialState:{
-        nowPlayingMovies:null,
-        nowPlayingTailer:null
+
+const movieSlice = createSlice({
+  name: "movies",
+  initialState: {
+    nowPlayingMovies: null,
+    popularMovies: null,
+    upcomingMovies: null,
+    trendingMovies: null,
+
+    nowPlayingTailer: null,
+  },
+  reducers: {
+    addNowPlayingMovies: (state, action) => {
+      state.nowPlayingMovies = action.payload;
     },
-    reducers:{
-        addNowPlayingMovies:(state,action)=>{
-            state.nowPlayingMovies=action.payload
-        },
-        addTrailer:(state,action)=>{
-            state.nowPlayingTailer=action.payload
-        }
-    }
-})
-export const {addNowPlayingMovies,addTrailer} = movieSlice.actions
-export default movieSlice.reducer
+    addPopularMovies: (state, action) => {
+      state.popularMovies = action.payload;
+    },
+    addUpcomingMovies: (state, action) => {
+      state.upcomingMovies = action.payload;
+    },
+    addTrendingMovies: (state, action) => {
+      state.trendingMovies = action.payload;
+    },
+
+    addTrailer: (state, action) => {
+      state.nowPlayingTailer = action.payload;
+    },
+  },
+});
+export const { addNowPlayingMovies, addTrailer, addPopularMovies, addUpcomingMovies, addTrendingMovies } = movieSlice.actions;
+export default movieSlice.reducer;
