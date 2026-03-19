ced22d0589eed3b0cc85ea6b46a581abf2713b58
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-03-19 22:22:53 +0530

    feat: update .gitignore to include environment files and refactor AI API key handling

--- FE-03/Week2-5/netflixgpt/src/utils/ai.ts
+++ FE-03/Week2-5/netflixgpt/src/utils/ai.ts
@@ -1,9 +1,9 @@
 import { GoogleGenAI } from "@google/genai";
-import { openAiKey } from "../constants/constant";
+
 
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
 const ai = new GoogleGenAI({
-    apiKey: openAiKey,
+    apiKey:import.meta.env.VITE_GEMINI_API_KEY,
 });
 
 export default ai;
