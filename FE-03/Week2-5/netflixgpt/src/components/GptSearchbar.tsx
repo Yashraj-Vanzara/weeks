d7da30f3ecba8f2ce1092bb271b1d38b9e87715d
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-03-19 19:30:27 +0530

    feat: add language configuration and update GptSearchbar for dynamic placeholders

--- FE-03/Week2-5/netflixgpt/src/components/GptSearchbar.tsx
+++ FE-03/Week2-5/netflixgpt/src/components/GptSearchbar.tsx
@@ -1,12 +1,16 @@
+import { useSelector } from "react-redux"
 import lang from "../constants/languageConstants"
+import type { RootState } from "../utils/appStore"
 
 const GptSearchbar = () => {
+  const langkey = useSelector((store: RootState) => store.config.lang) as keyof typeof lang
   return (
+
     <div className="pt-[10%] flex justify-center ">
         <form className="w-1/2   bg-black grid grid-cols-12">
-            <input className="col-span-9 p-4 m-4 text-black bg-white " type="text" placeholder={lang.hi.gptplaceholder} />
+            <input className="col-span-9 p-4 m-4 text-black bg-white " type="text" placeholder={lang[langkey].gptplaceholder} />
             <button className=" cursor-pointer py-2 px-4 m-4 bg-red-700 col-span-3 text-white rounded-lg">
-                {lang.hi.search}
+                {lang[langkey].search}
             </button>
         </form>
     </div>
--- FE-03/Week2-5/netflixgpt/src/components/Header.tsx
+++ FE-03/Week2-5/netflixgpt/src/components/Header.tsx
@@ -6,10 +6,13 @@ import { useEffect } from "react";
 import { addUser, removeUser } from "../utils/userSlice";
 import { setGpt } from "../utils/gptSlice";
 import { Supportedlang } from "../constants/constant";
+import { setLang } from "../utils/configSlice";
+import type { RootState } from "../utils/appStore";
 
 const Header = () => {
-  // @ts-expect-error Redux store type is not properly typed
-  const store = useSelector((store) => store.user);
+  
+  const store = useSelector((store:RootState) => store.user);
+  const gptstore = useSelector((store:RootState) => store.gpt.showGpt);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const handleSignout = () => {
@@ -44,6 +47,11 @@ const Header = () => {
   const handleGpt=()=>{
     dispatch(setGpt())
   }
+// @ts-expect-error if not e
+  const handleLanguage=(e)=>{
+    
+    dispatch(setLang(e.target.value))
+  }
   return (
     <div className="px-16 py-4 absolute z-99 w-full flex justify-between bg-linear-to-b from-[#212121] via-neutral-900 to-neutral-900/50 ">
       <img
@@ -53,11 +61,12 @@ const Header = () => {
       />
       {store && (
         <div className="flex gap-2  items-center">
-          <select className="p-2 bg-slate-400 text-white m-2" >
+          {gptstore &&  <select className="p-2 bg-slate-400 text-white m-2" onClick={handleLanguage} >
            {Supportedlang.map((lang=>(
             <option key={lang.code} value={lang.code}>{lang.name}</option>
            )))}
-          </select>
+          </select> }
+         
           <button onClick={handleGpt} className="px-2 py-2 cursor-pointer rounded-md text-white bg-purple-500">GptSearch</button>
           <img src="/user-icon.png" alt="user" />
           <p className="text-white">{store?.displayName}</p>
