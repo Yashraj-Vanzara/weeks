5f2591ebff483fea74e15d026a8807a232b8c21e
Yashraj Vanzara <vanzarayashraj@gmail.com>
2026-03-25 09:07:58 +0530

    feat: migrate App.js to App.tsx and update routing components

--- FE-03/Week2-5/FoodDeliveryApp/src/App.tsx
+++ FE-03/Week2-5/FoodDeliveryApp/src/App.tsx
@@ -1,17 +1,17 @@
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./components/Header";
-import Body from "./components/Body";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Error from "./components/Error";
-import Cart from "./components/Cart";
-import RestaurantMenu from "./components/RestaurantMenu";
+import Header from "./components/Header.js";
+import Body from "./components/Body.js";
+import About from "./components/About.js";
+import Contact from "./components/Contact.js";
+import Error from "./components/Error.js";
+import Cart from "./components/Cart.js";
+import RestaurantMenu from "./components/RestaurantMenu.js";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
-import {store} from "./utils/appStore"
+import {store} from "./utils/appStore.js"
 
-const Grocery=lazy(()=>import("./components/Grocery"))
+const Grocery=lazy(()=>import("./components/Grocery.js"))
 // App layout
 const AppLayout = () => {
   return (
